e0e984474f4d08bd5eec3fbecc5db593d605184c
Amir-FF <amir.computer.04.ir@gmail.com>
2026-08-27 22:08:41 +0330

    add  static asset handling

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ import classes from "./css/styles.module.css"
 import "./sass/main.scss"
 // import "bootstrap/dist/css/bootstrap.min.css"
 // import "bootstrap/dist/js/bootstrap.bundle.min.js"
-
 // import * as bootstrap from "bootstrap"
 import { Modal } from "bootstrap"
+// import imgUrl from "/images/img.jpg"
 
-console.log('Test', classes);
 
+// /////////////////////////////////////////////////
 document.querySelector('h2').className = classes.green;
 
 
@@ -18,3 +18,10 @@ const modal = new Modal('#exampleModal')
 document.getElementById('showModal').addEventListener('click', () => {
   modal.show()
 })
+
+
+///////////////////////////////////////////////////////////////////
+const imgUrl = new URL('/images/img.jpg', import.meta.url).href;
+
+
+document.getElementById('img').src = imgUrl;
