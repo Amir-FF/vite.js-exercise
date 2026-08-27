33071943db0dc5c4bc659e393373763580465ecb
Amir-FF <amir.computer.04.ir@gmail.com>
2026-08-27 20:42:07 +0330

    add bootstap

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 import "./css/main.css"
 import classes from "./css/styles.module.css"
 import "./sass/main.scss"
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
+// import * as bootstrap from "bootstrap"
+import { Modal } from "bootstrap"
 
 console.log('Test', classes);
 
 document.querySelector('h2').className = classes.green;
+
+
+// bootsrtap
+const modal = new Modal('#exampleModal')
+
+document.getElementById('showModal').addEventListener('click', () => {
+  modal.show()
+})
